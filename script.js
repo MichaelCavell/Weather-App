eb96f929a6dd84f27f83886e45ab36581d607d5a
MichaelCavell <96754228+MichaelCavell@users.noreply.github.com>
2023-11-03 18:10:55 -0400

    Write the functions that process the JSON data from the API and return an object with only the data for app.

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const city = 'Miami';
 const key = '8f9deabd1cb74de787c191736232610';
 const weatherUrl = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`;
+const weatherObject = {
+    condition: '',
+    cTemp: '',
+    fTemp: '',
+}
 
 async function checkWeather() {
     const request = await fetch(weatherUrl, {mode: 'cors'})
     const locationWeather = await request.json();
-    console.log(locationWeather);
-    console.log(locationWeather.current.condition.text);
-    console.log(locationWeather.current.temp_c);
-    console.log(locationWeather.current.temp_f);
     return locationWeather;
 }
 
-checkWeather();
+async function weatherPush() {
+    const weatherDetails = await checkWeather();
+    weatherObject.condition = weatherDetails.current.condition.text 
+    weatherObject.cTemp = weatherDetails.current.temp_c
+    weatherObject.fTemp = weatherDetails.current.temp_f
+    console.log(weatherObject);
+}
+
+weatherPush();
