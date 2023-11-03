378ae29914fbfadad80f30e78bc9643533e7f771
MichaelCavell <96754228+MichaelCavell@users.noreply.github.com>
2023-11-03 18:56:42 -0400

    Set up a simple form that will let users input their location and will fetch the weather info (still just console.log() it).

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const city = 'Miami';
+let city = 'Miami';
 const key = '8f9deabd1cb74de787c191736232610';
-const weatherUrl = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`;
+const form = document.getElementById('searchForm');
+const img = document.querySelector('img');
+img.src = 'https://media.giphy.com/media/QBjok2NBIzSR7IaDQK/giphy.gif'
+let weatherUrl = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`;
 const weatherObject = {
+    location: '',
     condition: '',
     cTemp: '',
     fTemp: '',
+}
+
+function constructURL() {
+    weatherUrl = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`;
 }
 
 async function checkWeather() {
@@ -15,6 +23,7 @@ async function checkWeather() {
 
 async function weatherPush() {
     const weatherDetails = await checkWeather();
+    weatherObject.location = city
     weatherObject.condition = weatherDetails.current.condition.text 
     weatherObject.cTemp = weatherDetails.current.temp_c
     weatherObject.fTemp = weatherDetails.current.temp_f
@@ -22,3 +31,40 @@ async function weatherPush() {
 }
 
 weatherPush();
+
+function submitForm(event) {
+  event.preventDefault();
+  city = form.elements.search.value;
+  constructURL();
+  weatherPush()
+  // fetchImage();
+  // form.elements.search.value = '';
+}
+
+form.addEventListener('submit', submitForm);
+
+
+
+  /*
+  function fetchImage() {
+  fetch(fetchURL, {mode: 'cors'})
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.data.images && response.data.images.original.url) {
+      img.src = response.data.images.original.url;
+      } else if (response.meta.msg == "Unauthorized") {
+        img.src = 'https://media.giphy.com/media/BsQAVgY6ksvIY/giphy.gif'
+      } else {
+        img.src = 'https://media.giphy.com/media/xGdvlOVSWaDvi/giphy.gif'
+      }
+    })
+    .catch(() => {
+      img.src = 'https://media.giphy.com/media/BsQAVgY6ksvIY/giphy.gif'
+    })
+  }
+  */
+
+
+
+
+
